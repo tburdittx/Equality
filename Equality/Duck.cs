5e994715a9ee2bb219f5a9a42808cc6bde988e5f
Tara <tburdittx@live.com>
2018-02-23 15:47:29 +0000

    ducks again

---
+++ Equality/Duck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equality
+{
+    public class Duck
+    {
+
+        Dictionary<Duck, int> ducks = new Dictionary<Duck, int>();
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int WeightInGrams { get; set; }
+        public int AgeInMonths { get; set; }
+
+        public List<KeyValuePair<Duck, int>> sorted;
+        public Duck(string name, string type, int weightInGrams)
+        {
+            Name = name;
+            Type = type;
+            WeightInGrams = weightInGrams;
+        }
+
+        public string CreatingDictionary()
+        {
+            ducks.Add(new Duck("Sir Quackalot", "Mallard", 100), 17);
+            ducks.Add(new Duck("Peter", "decoy", 90), 5);
+            ducks.Add(new Duck("Waddles", "Mallard", 150), 12);
+            ducks.Add(new Duck("Pickle", "Mallard ", 63), 33);
+
+            sorted = (from dd in ducks orderby dd.Value select dd).ToList();
+
+            return sorted.ToString();
+            
+        }
+        public Duck()
+        {
+
+        }
+
+    }
+}
---
+++ Equality/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck quack = new Duck();
+
+            quack.CreatingDictionary();
+
+            Console.WriteLine("Ducks sorted from youngest to oldest: \n");
+
+            foreach (KeyValuePair<Duck, int> duck in quack.sorted)
+            {
+                Console.WriteLine(duck.Key.Name.ToString() + ", Months Old: " + duck.Value.ToString() + ", Duck type: " + duck.Key.Type.ToString() + ", Duck weight: " + duck.Key.WeightInGrams.ToString() + "\n");
+            }
+
+            Console.WriteLine("End of ducks!!");
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
